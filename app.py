4b06bac301560c2078e313a85f88ef7308f84a26
IrineCKimeto <irenekimetoc@gmail.com>
2025-01-30 17:05:55 +0300

    Make changes

--- app.py
+++ app.py
@@ -9,23 +9,24 @@ load_dotenv()
 
 db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
+
+app = Flask(__name__)
+CORS(app)
 
     
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     
-    db.init_app(app)
+db.init_app(app)
 
-    with app.app_context():
-        from models import User, Book, Review  
-        db.create_all()
+with app.app_context():
+    from models import User, Book, Review  
+    db.create_all()
 
     
-    from routes import api_routes
-    app.register_blueprint(api_routes)
+from routes import api_routes
+app.register_blueprint(api_routes)
 
-    return app
+if __name__ == "__main__":
+    app.run(debug=True)
